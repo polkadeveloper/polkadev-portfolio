a0e29c6b365b95f6de0ba781cc10c8d2e8196991
alvarob27 <alvarobarcena27@gmail.com>
2024-02-23 21:29:24 +0100

    Agregadas validaciones de entrada en el formulario de correo electrónico

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import fs from 'node:fs'
 import { Resend } from 'resend'
 import { RESEND_TOKEN } from './env/tokens.js'
+import { type } from 'node:os'
 
 const resend = new Resend(RESEND_TOKEN)
 const PORT = process.env.PORT || 3000
@@ -20,6 +21,33 @@ app.get('/', (req, res) => {
 app.post('/email', async (req, res) => {
   const { nombre, apellido, segundoApellido, telefono, email, mensaje } =
     req.body
+    
+  // Validación del nombre y apellidos: no más de 50 caracteres
+  if (
+    nombre.length > 50 ||
+    apellido.length > 50 ||
+    segundoApellido.length > 50
+  ) {
+    return res
+      .status(400)
+      .json({
+        error: 'El nombre y los apellidos no pueden tener más de 50 caracteres',
+      })
+  }
+
+  // Validación del teléfono: solo dígitos y exactamente 9
+  const telefonoRegex = /^\d{9}$/
+  if (!telefonoRegex.test(telefono)) {
+    return res
+      .status(400)
+      .json({ error: 'El teléfono debe tener exactamente 9 dígitos' })
+  }
+
+  // Validación del correo electrónico: formato básico de correo electrónico
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+  if (!emailRegex.test(email)) {
+    return res.status(400).json({ error: 'El correo electrónico no es válido' })
+  }
   const { data, error } = await resend.emails.send({
     from: 'PolkaDev <onboarding@resend.dev>',
     to: ['alvarobarcena27@gmail.com'],
