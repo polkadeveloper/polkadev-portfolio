721fef7e347b2edbabb3af560d3ac8fe127a9c98
alvarob27 <alvarobarcena27@gmail.com>
2024-02-24 01:09:01 +0100

    Agrega campo de empresa en el formulario de email

--- server.js
+++ server.js
@@ -19,20 +19,25 @@ app.get('/', (req, res) => {
 })
 
 app.post('/email', async (req, res) => {
-  const { nombre, apellido, segundoApellido, telefono, email, mensaje } =
-    req.body
-    
+  const {
+    nombre,
+    apellido,
+    segundoApellido,
+    telefono,
+    empresa,
+    email,
+    mensaje,
+  } = req.body
+
   // Validación del nombre y apellidos: no más de 50 caracteres
   if (
     nombre.length > 50 ||
     apellido.length > 50 ||
     segundoApellido.length > 50
   ) {
-    return res
-      .status(400)
-      .json({
-        error: 'El nombre y los apellidos no pueden tener más de 50 caracteres',
-      })
+    return res.status(400).json({
+      error: 'El nombre y los apellidos no pueden tener más de 50 caracteres',
+    })
   }
 
   // Validación del teléfono: solo dígitos y exactamente 9
@@ -56,6 +61,7 @@ app.post('/email', async (req, res) => {
       <h2>Nombre y apellidos: ${nombre} ${apellido} ${segundoApellido}</h2>
       <h2><strong>Teléfono: ${telefono}</strong></h2>
       <h2><strong>Email: ${email}</strong></h2>
+      <h2><strong>Empresa: ${empresa}</strong></h2>
       <h2><strong>Mensaje: ${mensaje}</strong></h2>
     `,
   })
